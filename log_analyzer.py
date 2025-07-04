9188c4fbf9d8360e1dab3a60e2c1bf15c15cb0f6
haCkd0t <77449394+haCkd0t@users.noreply.github.com>
2025-07-04 15:58:00 +0530

    Add files via upload

---
+++ log_analyzer.py
@@ -0,0 +1,111 @@
+from itertools import count
+import time
+import json
+Clean_logs = []
+flagged_Ips = []
+
+with open ("generated_access.log","r") as f:
+    for line in f.readlines():
+        line = line.split()
+        dic = {
+        }
+        dic["IP"] = line[0]
+        dic["Data and Time"] = line[3].replace('[','')
+        dic["Endpoint"] = line[6].replace('[','')
+        dic["Method"] = line[5].replace('"','')
+        dic["Status"] = line[8]
+        dic["Size"] = line[9]
+        dic["User"] = line[10].replace('"','')
+        Clean_logs.append(dic)
+
+
+def view_logs():
+    print(json.dumps(Clean_logs,indent=4))
+
+
+def bruteforce_attack():
+    attempts = {}
+    for i in Clean_logs:
+        ip = i["IP"]
+        if i["Endpoint"] == "/login" and i["Status"] == "401":
+            if ip not in attempts:
+                attempts[ip] = 1
+            else:
+                attempts[ip] += 1
+    for ip, count in attempts.items():
+        if count > 3:
+            print(f"Suspicious IP: {ip} with {count} failed attempts")
+            flagged_Ips.append(ip)
+        else:
+            print("Nothing Suspicious here")
+
+def Suspicious_URL_Access():
+    suspicious_paths = ["/admin", "/wp-login", "/phpmyadmin", "/.env", "/shell", "/config"]
+    for i in Clean_logs:
+        url_t = {}
+        ip = i["IP"]
+        if i["Endpoint"] in suspicious_paths:
+            url_t[ip] = i["Endpoint"]
+            print(f"{ip} Tried to Access '{i['Endpoint']}' on {i['Data and Time']}")
+            flagged_Ips.append(ip)
+        else:
+            print("Nothing Suspicious here")
+
+
+def suspicious_Method():
+    suspicious_methods = ["PUT", "DELETE", "OPTIONS", "TRACE", "CONNECT"]
+    for i in Clean_logs:
+        ip = i["IP"]
+        if i["Method"] in suspicious_methods:
+            print(f"{ip} Tried Method {i['Method']} on Endpoint {i['Endpoint']}")
+        else:
+            print("Nothing Suspicious here")
+
+
+def FloodAttack():
+    rqst = {}
+    for i in Clean_logs:
+        ip = i["IP"]
+        if ip not in rqst:
+            rqst[ip] = 1
+        else:
+            rqst[ip] += 1
+    for key,value in rqst.items():
+        if value > 100:
+            print(f"{key} Sent {value} Requests to the Server")
+    else:
+        print("Nothing Suspicious here")
+
+
+def main():
+    while True:
+        print("Welcome to log Analyzer! Please Select Any One Operation: ")
+        print("1. To Display the Server Logs.")
+        print("2. To Analyze BruteForce Attack.")
+        print("3. To Analyze Suspicious URL Access.")
+        print("4. To Analyze Usage of Suspicious Methods.")
+        print("5. To Detect Flood Attacks.")
+        print("6. To Exit")
+        choice = int(input("Enter your choice: "))
+        print("Please wait", end="", flush=True)
+        for i in range(3):
+            print(".", end="", flush=True)
+            time.sleep(1)
+        print("\nProcessing Complete!")
+        if choice == 1:
+            view_logs()
+        elif choice == 2:
+            bruteforce_attack()
+        elif choice == 3:
+            Suspicious_URL_Access()
+        elif choice == 4:
+            suspicious_Method()
+        elif choice == 5:
+            FloodAttack()
+        elif choice == 6:
+            break
+        else:
+            print("Invalid Argument")
+            break
+
+main()
